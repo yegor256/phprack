e1c71bd89f770d4023c4a07e1ba8d7582515171b
Yegor Bugayenko <yegor@tpc2.com>
2010-04-07 08:57:21 +0000

    #refs #28 - obsolete comment removed

--- test/integration-tests/Disc/TailfTest.php
+++ test/integration-tests/Disc/TailfTest.php
@@ -10,10 +10,6 @@ require_once PHPRACK_PATH . '/Test.php';
 
 class TailfTest extends phpRack_Test
 {
-    /**
-     * Test with offset added in unit tests
-     * {@see phpRack_Package_Disc_File_TailfTest::testTailfWithOffset()}
-     */
     public function testLiveTail()
     {
         $fileName = '../test/phpRack/Package/Disc/_files/1000lines.txt';
