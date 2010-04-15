e49a0837786719a12f077cb7a4e6e94b5b400a55
Adrian Galewski <netcoderpl@gmail.com>
2010-04-15 20:40:53 +0000

    #47: implemented

---
+++ test/integration-tests/ConfigTest.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @version $Id$
+ */
+
+/**
+ * @see phpRack_Test
+ */
+require_once PHPRACK_PATH . '/Test.php';
+
+class ConfigTest extends phpRack_Test
+{
+
+    public function testConfigIni()
+    {
+        /**
+         * @see phpRack_Adapters_Config_Ini
+         */
+        require_once PHPRACK_PATH . '/Adapters/Config/Ini.php';
+        $ini = new phpRack_Adapters_Config_Ini(
+            PHPRACK_PATH . '/../test/phpRack/Adapters/Config/_files/app.ini',
+            'production'
+        );
+        $this->assert->db->mysql
+            ->connect(
+                'localhost',
+                3306,
+                $ini->resources->db->params->username,
+                $ini->resources->db->params->password
+            )
+            ->dbExists($ini->resources->db->params->dbname)
+            ->showConnections();
+    }
+
+}
