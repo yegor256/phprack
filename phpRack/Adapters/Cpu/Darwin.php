c2f4dafe28b1a8f6567dde4a5e0a977d08669249
Yegor Bugayenko <yegor@tpc2.com>
2010-04-26 17:53:53 +0000

    refs #17 - CPU frequency calculator implemented for Mac OS (Darwin)

--- phpRack/Adapters/Cpu/Darwin.php
+++ phpRack/Adapters/Cpu/Darwin.php
@@ -45,14 +45,12 @@ class phpRack_Adapters_Cpu_Darwin extends phpRack_Adapters_Cpu_Abstract
      * Get CPU BogoMips
      *
      * @return float
-     * @throws Exception If unable to get BogoMips
      * @see phpRack_Package_Cpu_Performance::atLeast()
      * @see phpRack_Adapters_Cpu_Abstract::getBogoMips()
-     * @todo #17 Should be implemented
      */
     public function getBogoMips()
     {
-        throw new Exception('Darwin::getBogoMips() not implemented yet');
+        return $this->getCpuFrequency() * 2;
     }
 
     /**
@@ -62,10 +60,27 @@ class phpRack_Adapters_Cpu_Darwin extends phpRack_Adapters_Cpu_Abstract
      * @throws Exception If can't get cpu frequency
      * @see getBogoMips()
      * @see phpRack_Adapters_Cpu_Abstract::getCpuFrequency()
-     * @todo #17 Should be implemented
      */
     public function getCpuFrequency()
     {
-        throw new Exception('Darwin::getCpuFrequency() not implemented yet');
+        /**
+         * @see phpRack_Adapters_Shell_Command
+         */
+        require_once PHPRACK_PATH . '/Adapters/Shell/Command.php';
+        $data = phpRack_Adapters_Shell_Command::factory(
+            'system_profiler SPHardwareDataType -xml 2>&1'
+        )->run();
+        $xml = simplexml_load_string($data);
+        if (!($xml instanceof SimpleXMLElement)) {
+            throw new Exception("Invalid result from system_profiler: '{$data}'");
+        }
+        $nodes = $xml->xpath('//string[preceding-sibling::key="current_processor_speed"]');
+        $node = strval($nodes[0]);
+        
+        if (strpos($node, 'GHz') === false) {
+            throw new Exception("Strange frequency from system_profiler: '{$node}'");
+        }
+        
+        return floatval($node) * 1000;
     }
 }
