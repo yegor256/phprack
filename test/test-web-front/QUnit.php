c72d8df6e070cb837b9ac7f17c3c8fe3da4ab57c
Yegor Bugayenko <yegor@tpc2.com>
2010-07-29 09:43:07 +0000

    configured and explained

--- test/test-web-front/QUnit.php
+++ test/test-web-front/QUnit.php
@@ -1,33 +1,42 @@
 <html>
-<head>
-<link rel="stylesheet" href="QUnit/qunit.css" type="text/css" />
-<script type="text/javascript" src="../phpRack/layout/jquery-1.4.2.min.js"></script>
-<script type="text/javascript">
-    var phpParams = {
-        calls: [],
-        ok: 'OK',
-        failure: 'FAILURE',
-        requestUri: 'QUnit/response/simple.js',
-        ajaxTag: 'test',
-        ajaxToken: 'token',
-    };
-</script>
-<script type="text/javascript" src="../phpRack/layout/index.js"></script>
-<script type="text/javascript" src="QUnit/qunit.js"></script>
-<?php
 
-$jsTestsPath = "QUnit/test/";
-$iterator = new RecursiveDirectoryIterator($jsTestsPath);
-foreach (new RecursiveIteratorIterator($iterator) as $file) :
+    <head>
+        <link rel="stylesheet" href="QUnit/qunit.css" type="text/css" />
 
-?>
+        <script type="text/javascript">
+            <?php echo file_get_contents("../../phpRack/layout/jquery-1.4.2.min.js")?>
+        </script>
 
-<script type="text/javascript" src="<?php echo str_replace('\\', '/', $file->getPathname()); ?>"></script>
+        <script type="text/javascript">
+            var phpParams = {
+                calls: [],
+                ok: 'OK',
+                failure: 'FAILURE',
+                requestUri: 'QUnit/response/simple.js',
+                ajaxTag: 'test',
+                ajaxToken: 'token',
+            };
+        </script>
+    
+        <script type="text/javascript">
+            <?php echo file_get_contents("../../phpRack/layout/index.js")?>
+        </script>
+    
+        <script type="text/javascript" src="QUnit/qunit.js"></script>
+    
+        <?php
+        $jsTestsPath = "QUnit/test/";
+        $iterator = new RecursiveDirectoryIterator($jsTestsPath);
+        ?>
+        <?php foreach (new RecursiveIteratorIterator($iterator) as $file): ?>
+            <script type="text/javascript" 
+                src="<?php echo str_replace('\\', '/', $file->getPathname()); ?>">
+            </script>
+        <?php endforeach; ?>
+    </head>
 
-<?php endforeach; ?>
-</head>
-<body>
-<ol id="qunit-tests"></ol>
+    <body>
+        <ol id="qunit-tests"></ol>
+    </body>
 
-</body>
 </html>
